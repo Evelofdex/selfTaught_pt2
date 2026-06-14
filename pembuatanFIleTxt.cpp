bc8313c73f040b71c1c28b585bfb3c8658b8af17
Evelofdex <evelofdex@gmail.com>
2026-06-14 18:27:19 +0700

    update semua, ada file yg ganti nama

--- pembuatanFIleTxt.cpp
+++ pembuatanFIleTxt.cpp
@@ -13,6 +13,17 @@ class textFileMaker{
 int main(){
 
     string namaFile;
+    char pilihan;
+
+    cout << "text file maker\n";
+    cout << "======================================\n";
+    cout << "1. tambah file\n";
+    cout << "2. isi teks kedalam suatu file\n";
+    cout << "3. output isi teks dalam suatu file\n";
+    cout << "======================================\n";
+    cout << "pilihan: ";
+    cin >> pilihan;
+    
 
     cout << "Masukan nama file: ";
     cin >> namaFile;
