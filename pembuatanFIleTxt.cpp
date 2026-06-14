3ca333d155d5b7e7a7a5a75c17d8c7b6ac034bfc
Evelofdex <evelofdex@gmail.com>
2026-06-14 22:05:34 +0700

    pembuatan fungsi membuat file baru tahap awal

--- pembuatanFIleTxt.cpp
+++ pembuatanFIleTxt.cpp
@@ -1,36 +1,80 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 using namespace std;
 
 class textFileMaker{
     private:
     string text;
+    string namaFile;
+    vector<string> listNamaFile;
 
     public:
+    void setText(string t){ text = t; }
 
+    void tambahFile(){
+        cout << "Masukan nama file yg akan dibuat ntar(tanpa spasi)\n";
+        cout << "-: ";
+        cin >> namaFile;
+        //cek nama file duplikat
+        if (!listNamaFile.empty()){
+            for (int i = 0; i <= listNamaFile.size(); i++){
+                if (namaFile == listNamaFile.at(i)){
+                    cout << "File dengan nama '" << listNamaFile.at(i) << "' sudah ada/dah dibuat sebelumnya\n";
+                    cout << "Gunakan nama lain\n\n";
+                    return;
+                }
+            }
+        }
+        //membuat file
+        ofstream file("folderFileText\\" + namaFile + ".txt");
+        //masukin nama file kedalam list
+        listNamaFile.push_back(namaFile);
+        cout << "text file dengan nama '" << namaFile << "' telah dibuat!\n";
+        //di close, karna pembuatan file dah beres
+        file.close();
+    }
+    void isiTeks(){
+
+    }
+    void outputText(){
+
+    }
+    void listFile(){
+
+    }
 };
 
 int main(){
-
-    string namaFile;
     char pilihan;
+    textFileMaker m;
 
-    cout << "text file maker\n";
-    cout << "======================================\n";
-    cout << "1. tambah file\n";
-    cout << "2. isi teks kedalam suatu file\n";
-    cout << "3. output isi teks dalam suatu file\n";
-    cout << "======================================\n";
-    cout << "pilihan: ";
-    cin >> pilihan;
+    do{
+        cout << "\ntext file maker\n";
+        cout << "======================================\n";
+        cout << "1. tambah file\n";
+        cout << "2. isi teks kedalam suatu file\n";
+        cout << "3. output isi teks dalam suatu file\n";
+        cout << "4. output isi teks dalam suatu file\n";
+        cout << "5. Exit\n";
+        cout << "======================================\n";
+        cout << "pilihan: ";
+        cin >> pilihan;
     
-
-    cout << "Masukan nama file: ";
-    cin >> namaFile;
-
-    ofstream inputFile("folderFileText\\" + namaFile + ".txt");
-
-
-    // close gile
-    inputFile.close();
+        switch (pilihan){
+            case '1':
+                m.tambahFile();
+                break;
+            case '2':
+                break;
+            case '3':
+                break;
+            case '4':
+                break;
+            case '5':
+                break;
+            default:
+                break;
+        }
+    } while (pilihan != '5');
 }
